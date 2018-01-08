be40c35ed18eee9c4c71d476cccf3dafffd6f728
Spring Trail <35170914+sprtg@users.noreply.github.com>
2018-01-08 02:33:23 +0100

    moved inline styles for header to stylesheet

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,32 +2,15 @@ import React from "react";
 import Link from "gatsby-link";
 
 const MenuLink = props =>
-  <li>
-    <a style={{ width: `calc(16.666% - 20px)`,
-                float: `left`,
-                marginRight: `10px`,
-                marginLeft: `10px`,
-                textAlign: `center`,
-                textDecoration: `none`,
-                color: `black`
-                }} href={props.to}>{props.children}</a>
+  <li className="mainmenuitem">
+    <a href={props.to}>{props.children}</a>
   </li>
 
 export default ({ children }) =>
-<div style={{ maxWidth: `64em`, marginTop: `0px`, marginRight: `auto`, marginLeft: `auto` }}>
-  <div class="clearfix" style={{ width: `calc(100% - 20px)`, 
-                height: `40px`,
-                marginRight: `10px`,
-                marginLeft: `10px`,
-                float: `left` }}>
-    <img style={{ width: `calc(25% - 10px`,
-                  height: `40px`,
-                  float: `left` }} src="http://lorempixel.com/200/40/" alt=""/>
-    <ul style={{ width: `calc(75% - 10px)`,
-                float: `left`,
-                listStyleType: `none`,
-                marginLeft: `10px`,
-                padding: 0}}>
+<div className="l-wrap">
+  <div className="header"> 
+    <img className="logo" src="http://lorempixel.com/200/40/" alt=""/>
+    <ul className="mainmenu">
       <MenuLink to="/">BLOG</MenuLink>
       <MenuLink to="/games/">STUFF</MenuLink>
       <MenuLink to="/about/">ABOUT</MenuLink>
